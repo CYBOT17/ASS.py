cd0b003c7eeb69ab4ed343b22042176cd738f8dd
CYBOT17 <princeemmanuel9898@gmail.com>
2024-12-05 00:14:07 +0100

    I did the optional assignment

---
+++ collections.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+food_list = ['rice', 'yam', 'banana', 'beans', 'garri', 'bread', 'sugar', 'gas', 'tea', 'meat']
+
+print(food_list)
+
+ingredient = input('Enter a food')
+for item in food_list:
+    if item == ingredient:
+        print(item, 'is in the list')
+
+total = 0
+for numbers in numbers:
+    total = total + numbers
