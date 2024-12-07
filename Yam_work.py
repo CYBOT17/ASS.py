6f6ca92d6b66f8bea703593288b5023ee2e84fe8
CYBOT17 <princeemmanuel9898@gmail.com>
2024-12-07 11:57:11 +0100

    I did the optional assignment again

---
+++ Yam_work.py
@@ -0,0 +1,7 @@
+def calculate_final_velocity(u,a,t):
+    print(u+a*t)
+u = float(input("initial velocity"))
+a = float(input("acceleration"))
+t = float(input("time"))
+calculate_final_velocity(u,a,t)
+
